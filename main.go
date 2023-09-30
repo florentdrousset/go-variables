7f9aa7bc2e21a4e383da1f27a50a06db5bdbbddb
Florent Drousset <florent.drousset@gmail.com>
2023-09-30 17:27:01 +0200

    more variables examples

--- main.go
+++ main.go
@@ -4,5 +4,15 @@ import "fmt"
 
 func main() {
   var age int = 35
-  fmt.Println(age)
+  var name string = "flo"
+  var height, weight int = 183, 62
+  var name2 = "bob"
+  var radius float32 = 2.4
+  email := "bob@go.org"
+
+  fmt.Println(age, name)
+  fmt.Println(name)
+  fmt.Println("Weight is", weight, "kg, and height is", height, "cm.")
+  fmt.Println(name2)
+  fmt.Println(email)
 }
